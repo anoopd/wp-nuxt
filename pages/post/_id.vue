c89692bc426f8bca58155547000710a763881489
Anoop D <anoopd1@hotmail.com>
2020-06-01 17:49:03 +0530

    First Commit

---
+++ pages/post/_id.vue
@@ -0,0 +1,17 @@
+<template>
+<section class="container">
+    <div>
+        <logo/>
+        <h1 class="title">Static Post Title</h1>
+        <span class="content">Static Post Content</span>
+    </div>
+</section>
+</template>
+<script>
+export default {
+
+}
+</script>
+<style>
+
+</style>
